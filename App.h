86881f7db31722eabf06b7a667de4518c15b5646
asy0 <58123184+asy0@users.noreply.github.com>
2023-03-18 23:05:04 +0100

    Update App.h

--- App.h
+++ App.h
@@ -1,14 +1,16 @@
 #ifndef STOCK_APP_H
 #define STOCK_APP_H
 
-
 #include "UI.h"
 #include "StockHashTable.h"
 
 class App {
 public:
+    // Runs the application
     void run();
+
 private:
+    // Instance of StockHashTable to store stocks
     StockHashTable stockHashTable;
 };
 
