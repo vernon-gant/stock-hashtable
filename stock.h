06087d8cee274a1d2147cf101b22b7ef8aa7cfb0
asy0 <58123184+asy0@users.noreply.github.com>
2023-03-18 23:08:28 +0100

    Update stock.h

--- stock.h
+++ stock.h
@@ -28,15 +28,20 @@ public:
 private:
     double open, high, low, close, adjClose;
 public:
+    // Default constructor
     PriceData();
 
+    // Parameterized constructor
     PriceData(std::string date, double open, double high, double low, double close, double adjClose) : date(std::move(
             date)), open(open), high(high), low(low), close(close), adjClose(adjClose) {}
 
+    // Print price data to console
     void print() const;
 
+    // Overloaded output stream operator
     friend std::ostream& operator<<(std::ostream &os, const PriceData &priceData);
 
+    // Overloaded input stream operator
     friend std::istream& operator>>(std::istream &is, PriceData &priceData);
 };
 
@@ -45,17 +50,17 @@ private:
     std::string name, shorthand, id;
     std::vector<PriceData> prices;
 
+    // Save prices to file
     void savePrices() const;
 public:
+    // Default constructor
     Stock();
 
-    ~Stock() {
-        prices.clear();
-    }
-
+    // Parameterized constructor
     Stock(std::string name, std::string shorthand, std::string id) : name(std::move(name)), shorthand(std::move(
             shorthand)), id(std::move(id)) {}
 
+    // Getter functions
     [[nodiscard]] const std::string &getName() const {
         return name;
     }
@@ -64,28 +69,37 @@ public:
         return shorthand;
     }
 
+    // Setter functions
     void setName(const std::string &name);
 
     void setShorthand(const std::string &shorthand);
 
     void setId(const std::string &id);
 
+    // Add price data to stock
     void addPriceData(const PriceData& priceData) {
         prices.push_back(priceData);
     }
 
+    // Import prices from file
     void importPrices(const std::string &path);
 
+    // Print stock prices to console
     void printPrice() const;
 
+    // Plot stock prices using matplotlib
     void plotPrice() const;
 
+    // Calculate stock signature
     [[nodiscard]] std::string signature() const;
 
+    // Overloaded equality operator
     bool operator==(const Stock &other) const;
 
+    // Overloaded output stream operator
     friend std::ostream &operator<<(std::ostream &os, const Stock &stock);
 
+    // Overloaded input stream operator
     friend std::istream &operator>>(std::istream &is, Stock &stock);
 };
 
@@ -94,11 +108,13 @@ private:
     std::shared_ptr<Stock> stock;
     bool deleted;
 public:
+    // Default constructor
     StockEntry() {
         stock = nullptr;
         deleted = false;
     }
 
+    // Getter functions
     [[nodiscard]] Stock &getStock() const {
         return *stock;
     }
@@ -111,10 +127,12 @@ public:
         return this->stock == nullptr;
     }
 
+    // Reset stock entry
     void reset(bool wasDeleted = false) {
         stock.reset();
         deleted = wasDeleted;
     }
+
 
     void setStock(std::shared_ptr<Stock> newStock) {
         this->stock = std::move(newStock);
